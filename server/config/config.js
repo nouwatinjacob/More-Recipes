9152e351f075bd715be6c637aa6daa2028169c22
sansaristic <jaysansa@gmail.com>
2017-09-11 08:43:20 +0100

    fix heroku

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ const config = {
     password: process.env.PASSWORD,
     database: process.env.DB_NAME,
     host: '127.0.0.1',
-    port: 5432,
+    port: 5433,
     dialect: 'postgres'
   },
   test: {
